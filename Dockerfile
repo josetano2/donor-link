0566eb9bd89be981a4963123f120861a688955a2
Tararaz <aurelius.wijaya@binus.ac.id>
2024-12-04 18:33:40 +0700

    feat: add docker

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
 # Use an official PHP runtime as a parent image
 FROM php:8.2-fpm
 
-# Set working directory
-WORKDIR /var/www/html
-
 # Install system dependencies
 RUN apt-get update && apt-get install -y \
     git \
@@ -13,20 +10,39 @@ RUN apt-get update && apt-get install -y \
     libxml2-dev \
     zip \
     unzip \
+    nodejs \
+    npm \
     && docker-php-ext-install pdo_mysql mbstring exif pcntl bcmath gd
 
 # Install Composer
 COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
 
-# Copy existing application directory contents
-COPY . /var/www/html
+# Set working directory
+WORKDIR /var/www/html
 
-# Copy existing application directory permissions
-COPY --chown=www-data:www-data . /var/www/html
+# Copy application code
+COPY . .
 
-# Change current user to www
-USER www-data
+# Install PHP dependencies
+RUN composer install --no-dev --optimize-autoloader --prefer-dist
 
-# Expose port 9000 and start php-fpm server
-EXPOSE 9000
-CMD ["php-fpm"]
+# Install Node.js dependencies and build assets
+RUN npm install
+RUN npm run prod
+
+# Clear and cache configurations
+RUN php artisan config:clear \
+    && php artisan config:cache \
+    && php artisan route:cache \
+    && php artisan view:cache \
+    && php artisan storage:link
+
+# Set permissions
+RUN chown -R www-data:www-data /var/www/html \
+    && chmod -R 755 /var/www/html/storage \
+    && chmod -R 755 /var/www/html/bootstrap/cache \
+    && chmod -R 755 /var/www/html/public
+
+# Expose port 8000 and start PHP built-in server
+EXPOSE 8000
+CMD ["php", "-S", "0.0.0.0:8000", "-t", "public"]
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,98 +1,99 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>{{ __('navbar.logo') }}</title>
-        <link rel="stylesheet" href="{{ asset('css/master.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/component.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/index.scss') }}">
-        <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
-        <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}" defer></script>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <title>{{ __('navbar.logo') }}</title>
+    <link rel="stylesheet" href="{{ asset('css/master.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/component.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/index.scss') }}">
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+    <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}" defer></script>
+</head>
 
-    <body class="parent-container">
-        <div class="navbar-container">
-            <div class="left-container">
-                <img src="logo.png" alt="">
-                <a class="logo-text">{{ __('navbar.logo') }}</a>
-            </div>
-            <div class="middle-container">
-                <a class="nav-text {{ Request::is('/') ? 'active' : '' }}"
-                   href="{{ route('home') }}">{{ __('navbar.home') }}</a>
-                <a class="nav-text {{ Request::is('events*') ? 'active' : '' }}"
-                   href="{{ route('events') }}">{{ __('navbar.events') }}</a>
-                <a class="nav-text {{ Request::is('tracker*') ? 'active' : '' }}"
-                   href="{{ route('tracker') }}">{{ __('navbar.tracker') }}</a>
-                <a class="nav-text {{ Request::is('request*') ? 'active' : '' }}"
-                   href="{{ route('request') }}">{{ __('navbar.request') }}</a>
-            </div>
-            <div class="right-container">
-                @auth
-                    <div class="dropdown">
-                        <button class="main-button primary-bg-color dropdown-toggle" type="button" id="userDropdown"
-                                                                                                   data-bs-toggle="dropdown" aria-expanded="false">
-                            {{ Auth::user()->name }}
-                        </button>
-                        <ul class="dropdown-menu" aria-labelledby="userDropdown">
-                            <li>
-                                <a class="dropdown-item" href="{{route('profile')}}">{{ __('navbar.profile') }}</a>
-                            </li>
-                            <li>
-                                <form action="{{ route('logout.account') }}" method="POST">
-                                    @csrf
-                                    <button type="submit" class="dropdown-item">{{ __('navbar.logout') }}</button>
-                                </form>
-                            </li>
-                        </ul>
-                    </div>
-                @else
-                    <a href="/login"><button type="button"
-                                             class="main-button primary-bg-color">{{ __('navbar.login') }}</button></a>
-                    <a href="/register">
-                        <button type="button" class="main-button secondary-bg-color">
-                            {{ __('navbar.register') }}
-                        </button>
-                    </a>
-                @endauth
+<body class="parent-container">
+    <div class="navbar-container">
+        <div class="left-container">
+            <img src="logo.png" alt="">
+            <a class="logo-text">{{ __('navbar.logo') }}</a>
+        </div>
+        <div class="middle-container">
+            <a class="nav-text {{ Request::is('/') ? 'active' : '' }}"
+                href="{{ route('home') }}">{{ __('navbar.home') }}</a>
+            <a class="nav-text {{ Request::is('events*') ? 'active' : '' }}"
+                href="{{ route('events') }}">{{ __('navbar.events') }}</a>
+            <a class="nav-text {{ Request::is('tracker*') ? 'active' : '' }}"
+                href="{{ route('tracker') }}">{{ __('navbar.tracker') }}</a>
+            <a class="nav-text {{ Request::is('request*') ? 'active' : '' }}"
+                href="{{ route('request') }}">{{ __('navbar.request') }}</a>
+        </div>
+        <div class="right-container">
+            @auth
                 <div class="dropdown">
-                    <button class="main-button secondary-bg-color dropdown-toggle" type="button" id="languageDropdown"
-                                                                                                 data-bs-toggle="dropdown" aria-expanded="false">
-                        {{ __('navbar.language') }}
+                    <button class="main-button primary-bg-color dropdown-toggle" type="button" id="userDropdown"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        {{ Auth::user()->name }}
                     </button>
-                    <ul class="dropdown-menu" aria-labelledby="languageDropdown">
-                        <li><a class="dropdown-item" href="{{ route('lang.switch', ['locale' => 'en']) }}">English</a></li>
-                        <li><a class="dropdown-item" href="{{ route('lang.switch', ['locale' => 'ja']) }}">Japanese</a></li>
+                    <ul class="dropdown-menu" aria-labelledby="userDropdown">
+                        <li>
+                            <a class="dropdown-item" href="{{route('profile')}}">{{ __('navbar.profile') }}</a>
+                        </li>
+                        <li>
+                            <form action="{{ route('logout.account') }}" method="POST">
+                                @csrf
+                                <button type="submit" class="dropdown-item">{{ __('navbar.logout') }}</button>
+                            </form>
+                        </li>
                     </ul>
                 </div>
+            @else
+                <a href="/login"><button type="button"
+                        class="main-button primary-bg-color">{{ __('navbar.login') }}</button></a>
+                <a href="/register">
+                    <button type="button" class="main-button secondary-bg-color">
+                        {{ __('navbar.register') }}
+                    </button>
+                </a>
+            @endauth
+            <div class="dropdown">
+                <button class="main-button secondary-bg-color dropdown-toggle" type="button" id="languageDropdown"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    {{ __('navbar.language') }}
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="languageDropdown">
+                    <li><a class="dropdown-item" href="{{ route('lang.switch', ['locale' => 'en']) }}">English</a></li>
+                    <li><a class="dropdown-item" href="{{ route('lang.switch', ['locale' => 'ja']) }}">Japanese</a></li>
+                </ul>
             </div>
-
         </div>
-        <div class="section-container">
-            @yield('content')
-        </div>
-        <footer>
-            <div class="footer-content">
-                <h3>Donor Link</h3>
-                <div class="footer-nav">
-                    <a href="/events">Events</a>
-                    <a href="/tracker">Tracker</a>
-                    <a href="/request">Request</a>
-                    <a href="/profile">Profile</a>
-                </div>
-            </div>
-            <hr />
-            <div class="footer-content">
-                <div class="bottom-text">
-                    Donate blood. Save lives. Make an impact.
-                </div>
-                <div class="bottom-text">
-                    © Donor Link 2024
-                </div>
-            </div>
 
-        </footer>
-    </body>
+    </div>
+    <div class="section-container">
+        @yield('content')
+    </div>
+    <footer>
+        <div class="footer-content">
+            <h3>Donor Link</h3>
+            <div class="footer-nav">
+                <a href="/events">Events</a>
+                <a href="/tracker">Tracker</a>
+                <a href="/request">Request</a>
+                <a href="/profile">Profile</a>
+            </div>
+        </div>
+        <hr />
+        <div class="footer-content">
+            <div class="bottom-text">
+                Donate blood. Save lives. Make an impact.
+            </div>
+            <div class="bottom-text">
+                © Donor Link 2024
+            </div>
+        </div>
+
+    </footer>
+</body>
 
 </html>
